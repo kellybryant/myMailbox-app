f0b6cf514e582fc486f71c37cfbc107c77fcb1b2
kellybryant <kbryant@walmartlabs.com>
2016-05-31 20:59:36 -0700

    Initial Commit

---
+++ myMailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myMailbox
+//
+//  Created by Kelly Bryant on 5/31/16.
+//  Copyright © 2016 Kelly Bryant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
